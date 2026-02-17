0ddf4524bdd6b7ca4c357871956be45bafd39363
Aykut <aykut.adem.0@gmail.com>
2026-02-17 15:55:45 +0300

    Started to consume backend

---
+++ Tripify/Controllers/CommentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tripify.Controllers
+{
+    public class CommentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tripify/Controllers/TourController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Tripify.Dtos.TourDtos;
+using Tripify.Services.TourServices;
+
+namespace Tripify.Controllers
+{
+    public class TourController : Controller
+    {
+        private readonly ITourService _tourService;
+
+        public TourController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        [HttpGet]
+        public IActionResult CreateTour()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTour(CreateTourDto createTourDto)
+        {
+            await _tourService.CreateTourAsync(createTourDto);
+            return RedirectToAction("TourList");
+        }
+
+        public async Task<IActionResult> TourList()
+        {
+            var values = await _tourService.GetAllTourAsync();
+            return View(values);
+        }
+    }
+}
