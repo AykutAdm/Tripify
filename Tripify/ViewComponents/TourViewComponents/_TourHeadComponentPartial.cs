692b1f94e20944f198d1258fced5c9e6ac45bdf6
Aykut <aykut.adem.0@gmail.com>
2026-02-19 14:17:43 +0300

    started to consume Tour entity

---
+++ Tripify/ViewComponents/TourViewComponents/_TourHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tripify.ViewComponents.TourViewComponents
+{
+    public class _TourHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tripify/ViewComponents/TourViewComponents/_TourScriptsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tripify.ViewComponents.TourViewComponents
+{
+    public class _TourScriptsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
